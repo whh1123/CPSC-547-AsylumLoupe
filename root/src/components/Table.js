f63173a894aa99ae4466adca1b1035450d1db140
Xin Wang <48210790+kokowaahmax@users.noreply.github.com>
2022-12-12 13:38:14 -0800

    finish tables

--- root/src/components/Table.js
+++ root/src/components/Table.js
@@ -1,55 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactEcharts from "echarts-for-react";
 import countryData from './result.json';
 
 export default function Table(props) {
     const {selectedView, origin, destination} = props;
-    var option = ""
+    var option = "";
+
+    const countries_to_iso = require('../hooks/countries_to_iso_a2.json');
+
+    const getCountryName = (iso) => {
+        return countries_to_iso.find((s) => s["code"] === iso)["name"]
+    }
+
     if (selectedView === "people"){
         if (destination) {
-            var prsentedData = countryData.filter((s) => s.geo === destination && s.sex === "T" && s.age === "TOTAL");
+            var presentedData = countryData.filter((s) => s.geo === destination && s.sex === "T" && s.age === "TOTAL");
             if(origin) {
-                prsentedData = prsentedData.filter((s) => s.citizen === origin);
+                presentedData = presentedData.filter((s) => s.citizen === origin);
             }
-            option = require("../hooks/people_bar_charts.json");
-            option["xAxis"][0]["data"] = ["2008", "2009", "2010", "2011",
-            "2012", "2013", "2014", "2015","2016", "2017", "2018", "2019",
-            "2020", "2021"];
+            var people_charts_option = require("../hooks/people_bar_charts.json");
 
-            var applicants = prsentedData.filter((s) => s.type === "ASY_APP").map((s) => s.numbers);
+            const applicants = presentedData.filter((s) => s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]));
+            people_charts_option.series[0].data = applicants;
 
-            for (let i = 0; i < applicants.length; i++ ) {
-               if (i !== 0) {
-                for (let j = 0; j < applicants[0].length; j++) {
-                    applicants[0][j] += applicants[i][j]
-                }
-               }
-            }
+            const resettles = presentedData.filter((s) => s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]),  [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            people_charts_option.series[1].data = resettles;
 
-            console.log(applicants[0]);
-
-            var resettles = prsentedData.filter((s) => s.type === "RES").map((s) => s.numbers);
-
-            for (let i = 0; i < resettles.length; i++) {
-                if (i !== 0) {
-                    for (let j = 0; j < resettles[0].length; j++) {
-                        resettles[0][j] += resettles[i][j]
-                    }
-                }
+            if (origin) {
+                people_charts_option.title.text = "Number of Asylum Applicants and Resettlement from "+ getCountryName(origin) +" Received by " + getCountryName(destination) + " in 2008 ~ 2021";
+            } else {
+                people_charts_option.title.text = "Number of Applicants and Resettlements Received by " + getCountryName(destination) + " in 2008 ~ 2021";
             }
 
-            console.log(resettles[0]);
-
-            option["series"][0]["data"] = applicants[0];
-            option["series"][1]["data"] = resettles[0];
+            option = people_charts_option;
 
         }
     } else if (selectedView === "gender") {
-        option = require("../hooks/gender_line_charts.json");
+        if (destination) {
+            var presentedData = countryData.filter((s) => s.geo === destination && s.age === "TOTAL");
+            if (origin) {
+                presentedData = presentedData.filter((s) => s.citizen === origin);
+            }
+            var gender_charts_option = require("../hooks/gender_line_charts.json");
+
+            const maleApp = presentedData.filter((s) => s.sex === "M" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]));
+            gender_charts_option.series[0].data = maleApp;
+
+            const maleRes = presentedData.filter((s) => s.sex === "M" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]));
+            gender_charts_option.series[1].data = maleRes;
+
+            const femaleApp = presentedData.filter((s) => s.sex === "F" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]));
+            gender_charts_option.series[2].data = femaleApp;
+
+            const femaleRes = presentedData.filter((s) => s.sex === "F" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]));
+            gender_charts_option.series[3].data = femaleRes;
+
+            const otherApp = presentedData.filter((s) => s.sex === "UNK" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            gender_charts_option.series[4].data = otherApp;
+
+            const otherRes = presentedData.filter((s) => s.sex === "UNK" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            gender_charts_option.series[5].data = otherRes;
+
+            if (origin) {
+                gender_charts_option.title.text = "Gender Distribution of Asylum Applicants and Resettlements from " + getCountryName(origin) + " Received by " + getCountryName(destination) + " in 2008 ~ 2021";
+            } else {
+                gender_charts_option.title.text = "Gender Distribution of Asylum Applicants and Resettlements Received by " + getCountryName(destination) + " in 2008 to 2021";
+            }
+            option = gender_charts_option;
+        }
     } else if (selectedView === "age"){
-        option = require("../hooks/age_bar_charts.json");
+        if (destination) {
+            var presentedData = countryData.filter((s) => s.geo === destination && s.sex === "T");
+            if (origin) {
+                presentedData = presentedData.filter((s) => s.citizen === origin);
+            }
+            var age_charts_option = require("../hooks/age_bar_charts.json");
+
+            const lt18_app = presentedData.filter((s) => s.age === "Y_LT18" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const lt35_app = presentedData.filter((s) => s.age === "Y18-34" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const lt65_app = presentedData.filter((s) => s.age === "Y35-64" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const ge65_app = presentedData.filter((s) => s.age === "Y_GE65" && s.type === "ASY_APP").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const lt18_res = presentedData.filter((s) => s.age === "Y_LT18" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const lt35_res = presentedData.filter((s) => s.age === "Y18-34" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const lt65_res = presentedData.filter((s) => s.age === "Y35-64" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+            const ge65_res = presentedData.filter((s) => s.age === "Y_GE65" && s.type === "RES").map((s) => s.numbers).reduce((acc, val) => acc.map((el, i) => el + val[i]), [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+
+            age_charts_option.series[0].data = lt18_app;
+            age_charts_option.series[1].data = lt35_app;
+            age_charts_option.series[2].data = lt65_app;
+            age_charts_option.series[3].data = ge65_app;
+            age_charts_option.series[4].data = lt18_res;
+            age_charts_option.series[5].data = lt35_res;
+            age_charts_option.series[6].data = lt65_res;
+            age_charts_option.series[7].data = ge65_res;
+
+            if (origin) {
+                age_charts_option.title.text = "Age Distribution of Asylum Applicants and Resettlements from " + getCountryName(origin) + " Received by " + getCountryName(destination) + " in 2008 ~ 2021";
+            } else {
+                age_charts_option.title.text = "Age Distribution of Asylum Applicants and Resettlements Received by " + getCountryName(destination) + " in 2008 to 2021";
+            }
+
+            option = age_charts_option;
+        }
     } else {
-        option = "";
+       option = "";
     }
 
     if (option) {
